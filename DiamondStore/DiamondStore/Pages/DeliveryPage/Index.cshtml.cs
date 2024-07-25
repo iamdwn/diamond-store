f390fe38dd1a3787cd8fa3d30bd5a849375629e4
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-07-25 23:16:20 +0700

    add pagination & search

--- DiamondStore/DiamondStore/Pages/DeliveryPage/Index.cshtml.cs
+++ DiamondStore/DiamondStore/Pages/DeliveryPage/Index.cshtml.cs
@@ -16,15 +16,35 @@ namespace DiamondStore.Pages.DeliveryPage
 
         public IList<DeliveryResponse> DeliveryList { get; set; } = default!;
         public string product { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(int currentPage = 1)
         {
+            //Check xem nguoi dang dang nhap co phai la admin khong
+            var UserRole = HttpContext.Session.GetInt32("UserRole");
+            if (UserRole != 4)
+            {
+                return Redirect("../Login");
+            }
+
             DeliveryList = await _deliveryService.GetDeliveryResponsesByAdmin();
 
             foreach (var delivery in DeliveryList)
             {
                 product = string.Join(",", delivery.Product);
             }
+
+            CurrentPage = currentPage;
+            int pageSize = 5;
+
+            int total = DeliveryList.Count();
+
+            TotalPages = (int)Math.Ceiling((double)total / pageSize);
+
+            DeliveryList = DeliveryList.Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
+
+            return Page();
         }
     }
 }
--- DiamondStore/DiamondStore/Pages/Shared/_StaffSidebar.cshtml.cs
+++ DiamondStore/DiamondStore/Pages/Shared/_StaffSidebar.cshtml.cs
@@ -7,6 +7,7 @@ namespace DiamondStore.Pages.Shared
     {
         public void OnGet()
         {
+           
         }
     }
 }
